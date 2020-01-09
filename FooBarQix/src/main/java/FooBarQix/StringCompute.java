75c8df019359f6cdbf228aee140765483972fde0
tonyknudson <tonyknudson91@gmail.com>
2020-01-08 20:52:19 -0600

    notes commit

--- FooBarQix/src/main/java/FooBarQix/StringCompute.java
+++ FooBarQix/src/main/java/FooBarQix/StringCompute.java
@@ -3,13 +3,19 @@ package FooBarQix;
 public class StringCompute {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Tokenize string
+		//Loop tokens through divisor and contains methods
+		//Add results to stringBuffer
+		//Output string buffer
 	}
 	
-	public String getFoo(String input) {
+	public String checkDivisor(String input) {
 		Integer number = Integer.parseInt(input);
-		return (number % 3 == 0 ? "Foo" : input);
+		//switch on modulo
+		String returnable = null;
+		// (number % 3 == 0 ? "Foo" : input);
+		 
+		 return returnable;
 	}
 
 }
