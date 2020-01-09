ebe96e1d6ce5a7dc9180bba5ba9ad88fca77abe8
tonyknudson <tonyknudson91@gmail.com>
2020-01-08 21:18:34 -0600

    need to modify code to account for multiple trues

--- FooBarQix/src/main/java/FooBarQix/StringCompute.java
+++ FooBarQix/src/main/java/FooBarQix/StringCompute.java
@@ -3,19 +3,58 @@ package FooBarQix;
 public class StringCompute {
 
 	public static void main(String[] args) {
+		String input = "21";
+		//Accept input
 		//Tokenize string
 		//Loop tokens through divisor and contains methods
 		//Add results to stringBuffer
 		//Output string buffer
+		//System.out.println(StringCompute.doMain(input));
 	}
 	
+	public String doMain(String input) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(checkDivisor(input));
+		buffer.append(checkContains(input));
+		
+		return buffer.toString();
+	}
+
 	public String checkDivisor(String input) {
-		Integer number = Integer.parseInt(input);
-		//switch on modulo
-		String returnable = null;
-		// (number % 3 == 0 ? "Foo" : input);
-		 
-		 return returnable;
+		
+		if (null != input) {
+			Integer number = Integer.parseInt(input);
+			System.out.println(input);
+			System.out.println(number % 3);
+			System.out.println(number % 7);
+			if (number % 3 == 0) {
+				return "Foo";
+			} else if (number % 5 == 0) {
+				return "Bar";
+			} else if (number % 7 == 0) {
+				return "Qix";
+			} else {
+				return input;
+			}
+		} else {
+			return "";
+		}
+	}
+	
+	public String checkContains(String input) {
+		if (null != input) {
+			if (input.contains("3")) {
+				return "Foo";
+			} else if (input.contains("5")) {
+				return "Bar";
+			} else if (input.contains("7")) {
+				return "Qix";
+			} else {
+				return input;
+			}
+		} else {
+			return "";
+		}
 	}
 
 }
--- FooBarQix/src/test/java/FooBarQix/StringComputeTest.java
+++ FooBarQix/src/test/java/FooBarQix/StringComputeTest.java
@@ -8,9 +8,15 @@ public class StringComputeTest extends TestCase{
 	protected StringCompute compute = new StringCompute();
 	
 	@Test
-	public void testExample(){
-		String result = compute.getFoo("3");
+	public void test_checkDivisor(){
+		String result = compute.checkDivisor("3");
 		assertEquals("Foo", result);
+	}
+	
+	@Test
+	public void test_numbers() {
+		String result = compute.doMain("21");
+		System.out.println(result);
 	}
 
 }
